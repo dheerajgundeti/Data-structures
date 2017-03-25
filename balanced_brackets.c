6f8c876966cecfbee7203b1333f0748ed12f43b7
dheerajgundeti <gundetidheeraj@gmail.com>
2017-03-25 18:57:18 +0530

    stacks

---
+++ balanced_brackets.c
@@ -0,0 +1,34 @@
+//hackerrank/datastructures/stacks
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
+
+int main(){
+    int t,i; 
+    scanf("%d",&t);
+    for(int a0 = 0; a0 < t; a0++){
+        char* s = (char *)malloc(10240 * sizeof(char));
+        scanf("%s",s);
+        int top=-1;
+        char c[1000];
+        
+        for(int i=0;s[i]!='\0';i++){
+            switch(s[i]){
+                case '(':
+                case '{':
+                case '[':top++;c[top]=s[i];break;
+                case ')':if(c[top]=='(')top--;break;
+                case '}':if(c[top]=='{')top--;break;
+                case ']':if(c[top]=='[')top--;break;
+            }
+        }
+        if(top==-1)printf("YES\n");
+        else printf("NO\n");
+    }
+    return 0;
+}
+
