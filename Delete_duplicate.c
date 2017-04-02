3ed1822834533d523e6adafb7e7f5fc16cae9ca3
dheerajgundeti <gundetidheeraj@gmail.com>
2017-04-02 10:25:27 +0530

    linked_list

---
+++ Delete_duplicate.c
@@ -0,0 +1,12 @@
+//hackerrank/data_structures/linkedlist
+Node* RemoveDuplicates(Node *head)
+{
+        Node *temp=head;
+        while(temp->next!=NULL){
+            if(temp->data==temp->next->data)
+                temp->next=temp->next->next;
+            else
+                temp=temp->next;
+         }
+        return head;     
+}
